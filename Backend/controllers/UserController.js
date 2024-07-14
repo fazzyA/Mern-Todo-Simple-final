d478fcdc5a851d3e608f048d115dd134a138c94b
fazzyA <faz.pak@gmail.com>
2024-07-14 12:48:00 +0500

    jwt and bcrypt

--- Backend/controllers/UserController.js
+++ Backend/controllers/UserController.js
@@ -1,5 +1,6 @@
 const User = require("../models/user");
 var bcrypt = require("bcryptjs");
+var jwt = require('jsonwebtoken');
 
 const createUser = async (req, res) => {
   const { name, email, password } = req.body;
@@ -17,21 +18,33 @@ const getUser = async (req, res) => {
   res.send({ status: 200, data: result });
 };
 const login = async (req, res) => {
+  console.log("iminlogin")
   const { email, password } = req.body;
   const user = await User.findOne({ email });
   if (!user) {
     res.send({ status: 404, data: "no user is found" });
   }
-  bcrypt.compare(password, user.password, function (err, res) {
-    if (err) {
-      res.send({ status: 404, data: "password is incorrect" });
+  bcrypt.compare(password, user.password, async function (err, resp) {
+    console.log(resp)
+    if (resp) {
+      // logic
+      const obj = {
+        id: user._id,
+        email: user.email, 
+        name: user.name
+      }
+      const token = await jwt.sign(obj, process.env.JWT_SECRET)
+      console.log("token", token)
+  
+      res.send({ status: 200, data: token, message: "user logged in" });
+    } else {
+      res.send({ status: 400, message: "user pass incorrect" });
     }
-    // logic
-    res.send({ status: 200, data: user });
   });
 };
 
 module.exports = {
   createUser,
   getUser,
+  login
 };
--- Backend/routes/User.js
+++ Backend/routes/User.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
-const { createUser, getUser } = require("../controllers/UserController");
+const { createUser, getUser, login } = require("../controllers/UserController");
 const router = Router();
 router.get("/", getUser)
 router.post("/", createUser);
+router.post("/login", login);
 module.exports = router
